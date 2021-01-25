6043952890c7babbafaea9fe243681519764f880
fatOrange <1300755523@qq.com>
2021-01-25 22:41:07 +0800

    dxgi debug info added

--- hw3d/Window.h
+++ hw3d/Window.h
@@ -1,33 +1,61 @@
+/******************************************************************************************
+*	Chili Direct3D Engine																  *
+*	Copyright 2018 PlanetChili <http://www.planetchili.net>								  *
+*																						  *
+*	This file is part of Chili Direct3D Engine.											  *
+*																						  *
+*	Chili Direct3D Engine is free software: you can redistribute it and/or modify		  *
+*	it under the terms of the GNU General Public License as published by				  *
+*	the Free Software Foundation, either version 3 of the License, or					  *
+*	(at your option) any later version.													  *
+*																						  *
+*	The Chili Direct3D Engine is distributed in the hope that it will be useful,		  *
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+*	GNU General Public License for more details.										  *
+*																						  *
+*	You should have received a copy of the GNU General Public License					  *
+*	along with The Chili Direct3D Engine.  If not, see <http://www.gnu.org/licenses/>.    *
+******************************************************************************************/
 #pragma once
 #include "ChiliWin.h"
 #include "ChiliException.h"
-#include "keyboard.h"
+#include "Keyboard.h"
 #include "Mouse.h"
 #include "Graphics.h"
 #include <optional>
 #include <memory>
 
 
-class Window 
+class Window
 {
 public:
-	class Exception :public ChiliException
+	class Exception : public ChiliException
+	{
+		using ChiliException::ChiliException;
+	public:
+		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+	class HrException : public Exception
 	{
 	public:
-		Exception(int line, const char* file, HRESULT hr)noexcept;
+		HrException(int line, const char* file, HRESULT hr) noexcept;
 		const char* what() const noexcept override;
-		virtual const char* GetType() const noexcept;
-		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
-		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
 	private:
 		HRESULT hr;
-
 	};
-
+	class NoGfxException : public Exception
+	{
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
+	};
 private:
-	//singletion manages registration/cleanup of window class
-	class  WindowClass 
+	// singleton manages registration/cleanup of window class
+	class WindowClass
 	{
 	public:
 		static const char* GetName() noexcept;
@@ -42,12 +70,12 @@ private:
 		HINSTANCE hInst;
 	};
 public:
-	Window(int width, int height, const char* name)noexcept;
+	Window(int width, int height, const char* name);
 	~Window();
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
-	void SetTitle(const std::string title);
-	static std::optional<int> ProcessMessages();
+	void SetTitle(const std::string& title);
+	static std::optional<int> ProcessMessages() noexcept;
 	Graphics& Gfx();
 private:
 	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
@@ -63,6 +91,8 @@ private:
 	std::unique_ptr<Graphics> pGfx;
 };
 
+
 // error exception helper macro
-#define CHWND_EXCEPT(hr) Window::Exception(__LINE__,__FILE__,hr)
-#define CHWND_LAST_EXCEPT() Window::Exception(__LINE__,__FILE__,GetLastError())
+#define CHWND_EXCEPT( hr ) Window::HrException( __LINE__,__FILE__,(hr) )
+#define CHWND_LAST_EXCEPT() Window::HrException( __LINE__,__FILE__,GetLastError() )
+#define CHWND_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
