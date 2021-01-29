5da4c66a36c9f7dbbeee866dd1fcdb006ca1a789
fatOrange <1300755523@qq.com>
2021-01-29 14:41:58 +0800

    multi cube

--- hw3d/VertexShader.cpp
+++ hw3d/VertexShader.cpp
@@ -1,7 +1,26 @@
-#include "VertexBuffer.h"
+#include "VertexShader.h"
+#include "GraphicsThrowMacros.h"
 
-void VertexBuffer::Bind(Graphics& gfx) noexcept
+
+VertexShader::VertexShader(Graphics& gfx, const std::wstring& path)
 {
-	const UINT offset = 0u;
-	GetContext(gfx)->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+	INFOMAN(gfx);
+
+	GFX_THROW_INFO(D3DReadFileToBlob(path.c_str(), &pBytecodeBlob));
+	GFX_THROW_INFO(GetDevice(gfx)->CreateVertexShader(
+		pBytecodeBlob->GetBufferPointer(),
+		pBytecodeBlob->GetBufferSize(),
+		nullptr,
+		&pVertexShader
+	));
+}
+
+void VertexShader::Bind(Graphics& gfx) noexcept
+{
+	GetContext(gfx)->VSSetShader(pVertexShader.Get(), nullptr, 0u);
+}
+
+ID3DBlob* VertexShader::GetBytecode() const noexcept
+{
+	return pBytecodeBlob.Get();
 }
