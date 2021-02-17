3baa2a4b8df296f8461e5cb17ab9f2372a639ad9
fatOrange <1300755523@qq.com>
2021-02-17 17:04:02 +0800

    23纹理映射

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -3,10 +3,15 @@
 #include "Melon.h"
 #include "Pyramid.h"
 #include "triangle_cube.h"
+#include "SkinBox.h"
 #include <memory>
 #include <algorithm>
 #include <iterator>
 #include "ChiliMath.h"
+#include "Surface.h"
+#include "GDIPlusManager.h"
+#include "Sheet.h"
+GDIPlusManager gdipm;
 
 App::App()
 	:
@@ -28,7 +33,7 @@ App::App()
 					odist, rdist
 					);
 			case 1:
-				return std::make_unique<Box>(
+				return std::make_unique<SkinBox>(
 					gfx, rng, adist, ddist,
 					odist, rdist, bdist
 					);
@@ -36,6 +41,11 @@ App::App()
 				return std::make_unique<Melon>(
 					gfx, rng, adist, ddist,
 					odist, rdist, longdist, latdist
+					);
+			case 3:
+				return std::make_unique<Sheet>(
+					gfx, rng, adist, ddist,
+					odist, rdist
 					);
 			default:
 				assert(false && "bad drawable type in factory");
@@ -52,7 +62,7 @@ App::App()
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 }; //选择类型
+		std::uniform_int_distribution<int> typedist{ 0,3 }; //选择类型
 	};
 
 	Factory f(wnd.Gfx());
@@ -88,7 +98,7 @@ void App::DoFrame()
 	wnd.Gfx().ClearBuffer(0.07f, 0.0f, 0.12f);
 	for (auto& d : drawables)
 	{
-		d->Update(dt);
+		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
 		d->Draw(wnd.Gfx());
 	}
 	wnd.Gfx().EndFrame();
